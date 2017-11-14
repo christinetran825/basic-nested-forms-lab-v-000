8285732487aa1e19bae339d7763e92d872e2461f
Christine Tran <christinetran825@gmail.com>
2017-11-14 22:48:26 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,11 +15,7 @@ class RecipesController < ApplicationController
 
   def create
     recipe = Recipe.create(recipe_params)
-    if @recipe.save
-      redirect_to recipe_path(@recipe)
-    else
-      render :new
-    end
+    
   end
 
 
