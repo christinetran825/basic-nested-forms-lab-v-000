a6b39a6a4f61405a74d4dcc2024cd1fb10cf376a
Christine Tran <christinetran825@gmail.com>
2017-11-14 22:32:44 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -22,7 +22,7 @@ class RecipesController < ApplicationController
   def recipe_params
     params.require(:recipe).permit(
       :title,
-      recipe_ingredients_attributes: [
+      ingredients_attributes: [
         :name,
         :quantity
       ]
